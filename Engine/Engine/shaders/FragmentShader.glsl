c8afaa76962e455d6762593c1353f4c02fb9c4d9
Logan Devillard <ldevillard@virtuosgames.com>
2024-03-12 14:35:10 +0100

    added texture + resizing window + now render a cube

--- Engine/Engine/shaders/FragmentShader.glsl
+++ Engine/Engine/shaders/FragmentShader.glsl
@@ -2,13 +2,12 @@
 
 out vec4 FragColor;  
 
-in vec3 ourColor;
+//in vec3 ourColor;
 in vec2 TexCoord;
 
 uniform sampler2D texture1;
-uniform sampler2D texture2;
 
 void main()
 {
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+	FragColor = texture(texture1, TexCoord);
 }
