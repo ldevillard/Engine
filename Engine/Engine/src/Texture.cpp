b61afe29e04d3128404ab9dbecceb87a450c6ec6
Logan Devillard <ldevillard@virtuosgames.com>
2024-03-13 12:43:26 +0100

    added Camera class + clean code

--- Engine/Engine/src/Texture.cpp
+++ Engine/Engine/src/Texture.cpp
@@ -1,5 +1,7 @@
 #include "Texture.h"
 
+#pragma region Public Methods
+
 Texture::Texture(const char* texturePath, std::string name, TextureParam params) : Name(name), ID(0)
 {
    glGenTextures(1, &ID);
@@ -12,7 +14,7 @@ Texture::Texture(const char* texturePath, std::string name, TextureParam params)
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
    // load image, create texture and generate mipmaps
    int width, height, nrChannels;
-   
+
    if (params.flip)
    {
       stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
@@ -23,15 +25,15 @@ Texture::Texture(const char* texturePath, std::string name, TextureParam params)
    {
       switch (params.format)
       {
-         case TextureFormat::RGB :
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-            break;
-         case TextureFormat::RGBA :
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-            break;
-         default :
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-            break;
+      case TextureFormat::RGB:
+         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+         break;
+      case TextureFormat::RGBA:
+         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+         break;
+      default:
+         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+         break;
       }
       glGenerateMipmap(GL_TEXTURE_2D);
    }
@@ -41,3 +43,5 @@ Texture::Texture(const char* texturePath, std::string name, TextureParam params)
    }
    stbi_image_free(data);
 }
+
+#pragma endregion
