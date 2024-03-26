c3a08e95f0757ee8459ba96ca304771cb9d54988
ldevillard <logandvllrd@gmail.com>
2024-03-26 21:55:51 +0100

    added wireframe sphere gizmo

---
+++ Engine/include/data/mesh/WireSphere.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Mesh.h"
+
+class WireSphere : public Mesh
+{
+public:
+	WireSphere();
+
+	void Draw(Shader* shader) override;
+
+private:
+	void generateCircle(std::vector<Vertex>& vertices, std::vector<unsigned int>& indices, float radius, int numVertices, const glm::vec3& axis);
+};
--- Engine/src/utils/Gizmo.cpp
+++ Engine/src/utils/Gizmo.cpp
@@ -17,8 +17,10 @@ void Gizmo::InitGizmos(Shader* s)
 {
 	shader = s;
 	gizmos[WireCubeGizmo] = std::make_unique<WireCube>();
+	gizmos[WireSphereGizmo] = std::make_unique<WireSphere>();
 }
 
+// Need to make a function for shader binding
 void Gizmo::DrawWireCube(const Color& color, const Transform& transform)
 {
 	if (!shader)
@@ -27,6 +29,30 @@ void Gizmo::DrawWireCube(const Color& color, const Transform& transform)
 		return;
 	}
 
+	bindShader(color, transform);
+
+	gizmos[WireCubeGizmo]->Draw(shader);
+}
+
+void Gizmo::DrawWireSphere(const Color& color, const Transform& transform)
+{
+	if (!shader)
+	{
+		std::cerr << "Gizmo shader is not initialized" << std::endl;
+		return;
+	}
+
+	bindShader(color, transform);
+
+	gizmos[WireSphereGizmo]->Draw(shader);
+}
+
+#pragma endregion
+
+#pragma region Private Methods
+
+void Gizmo::bindShader(const Color& color, const Transform& transform)
+{
 	shader->Use();
 
 	glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 1000.0f);
@@ -50,8 +76,6 @@ void Gizmo::DrawWireCube(const Color& color, const Transform& transform)
 	shader->SetMat4("model", model);
 
 	shader->SetVec3("color", color.Value);
-
-	gizmos[WireCubeGizmo]->Draw(shader);
 }
 
 #pragma endregion
