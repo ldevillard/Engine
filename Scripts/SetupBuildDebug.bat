e25fd5f6623e3adb0a715c5fffbf34c8806efa04
ldevillard <logandvllrd@gmail.com>
2024-04-13 01:59:12 +0200

    added build setup bat file

---
+++ Scripts/SetupBuildDebug.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set SRC_PATH=..\Engine\
+
+set DEST_PATH=..\x64\Debug\
+
+echo delete the shaders and resources folders from the destination folder
+rmdir /S /Q "%DEST_PATH%\shaders"
+rmdir /S /Q "%DEST_PATH%\resources"
+
+echo copy the shaders and resources folders from the source folder to the destination folder
+xcopy /E /I "%SRC_PATH%\shaders" "%DEST_PATH%\shaders"
+xcopy /E /I "%SRC_PATH%\resources" "%DEST_PATH%\resources"
+
+echo done
+pause
